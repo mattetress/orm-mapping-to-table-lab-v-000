b13bf22e70eeb5a21d38df7072e9758f7b0f62ff
Matt Etress <mattetress@me.com>
2018-10-17 11:19:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,9 @@
 class Student
-  attr_accessor :name, :grade, :id
+  attr_accessor :name, :grade
+  attr_reader :id
 
   def initialize(name, grade, id = nil)
-    @name, @grade, @id = name, grade, id 
+    @name, @grade, @id = name, grade, id
   end
 
 end
